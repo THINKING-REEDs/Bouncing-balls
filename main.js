5f21313588428f0a6399158bea0fe8bfb59212b4
lihua <54234853+THINKING-REEDs@users.noreply.github.com>
2020-08-23 23:07:01 +0800

    bouncing-balls
    JS OOP

---
+++ main.js
@@ -0,0 +1,175 @@
+// 设置画布
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+
+
+function random(min,max) {
+  const num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
+}
+function randomColor() {
+  return 'rgb(' +
+         random(0, 255) + ', ' +
+         random(0, 255) + ', ' +
+         random(0, 255) + ')';
+}
+class Shape {
+  constructor (x, y, velX, velY, exists) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.exists = exists;
+
+  }
+  draw () {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+  update () {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  collisionDetect () {
+    for (let j = 0; j < balls.length; j++) {
+      if (this !== balls[j]) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].color = this.color = randomColor();
+        }
+      }
+    }
+  }
+}
+class Ball extends Shape {
+  constructor (x, y, velX, velY, exists, color, size) {
+    super(x, y, velX, velY, exists);
+    this.color = color;
+    this.size = size;
+  }
+}
+class EvilCircle extends Shape {
+  constructor (x, y, exists) {
+    super(x, y, 20, 20, exists)
+    this.color = 'white';
+    this.size = 10;
+  }
+  draw () {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  checkBounds () {
+    if ((this.x + this.size) >= width) {
+      this.X -= this.X;
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.X += this.X;
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.Y -= this.Y;
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.Y += this.Y;
+    }
+  }
+  setControls () {
+    window.onkeydown = e => {
+      switch(e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+      }
+    };
+  }
+  collisionDetect () {
+    for (let j = 0; j < balls.length; j++) {
+      if (balls[j].exists) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].exists = false;
+        }
+      }
+    }
+  }
+}
+let balls = [];
+let evilCircle = new EvilCircle(
+  random(10, width - 10),
+  random(10, height - 10),
+  true
+)
+let counter = document.getElementById('counter');
+evilCircle.setControls();
+while (balls.length < 50) {
+    let size = random(10, 20);
+    let ball = new Ball(
+      random(0 + size, width - size),
+      random(0 + size, height - size),
+      random(-7, 7),
+      random(-7, 7),
+      true,
+      randomColor(),
+      size
+    );
+    balls.push(ball);
+}
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+  existsBalls = balls.filter( ball => ball.exists);
+  counter.textContent = existsBalls.length;
+  for (let i = 0; i < existsBalls.length; i++) {
+    existsBalls[i].draw();
+    existsBalls[i].update();
+    existsBalls[i].collisionDetect();
+  }
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
+  requestAnimationFrame(loop);
+}
+loop();
